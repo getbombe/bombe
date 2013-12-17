9fe7b2f397a879eb0f2670a246764efeb7ae2de7
Eddie Kim <eddotman@gmail.com>
2013-12-17 07:45:25 -0500

    calculus working

--- pycompute/lib/calculus.py
+++ pycompute/lib/calculus.py
@@ -8,6 +8,9 @@ def differentiate (data):
 
 	dy_dx = diff(y_dat) / diff(x_dat)
 
+	#Delete last pt because diff() makes dy_dx smaller by 1 element
+	x_dat = delete(x_dat, s_[:1])
+
 	return (x_dat, dy_dx)
 
 def integrate (data):
@@ -19,3 +22,22 @@ def integrate (data):
 	integral = trapz(y_dat, x_dat)
 
 	return integral
+
+
+#TESTS
+if __name__ == '__main__':
+
+	import matplotlib.pyplot as plt 
+
+	x = arange(0, 2*pi, 0.1)
+	y = sin(x)
+
+	deriv = differentiate([x, y])
+	intgr = integrate([x, y])
+
+	plt.plot (x, y)
+	plt.plot (deriv[0], deriv[1])
+
+	plt.show()
+
+	print intgr
