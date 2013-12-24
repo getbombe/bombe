7d97f141c0e1d497ab0078930c8a16df22c0d777
Eddie Kim <eddotman@gmail.com>
2013-12-24 14:42:21 -0500

    started parsing in python

--- pycompute/lib/parse.py
+++ pycompute/lib/parse.py
@@ -1,8 +1,14 @@
 import csv
 import numpy as np 
 
-def parse_data (data):
+def parse_data (data, fmt = None):
 	'''parses datafiles given by user'''
 
-	
+	#check if format is known
+	if fmt == 'csv':
+		pass
+	else:
+		pass
+
+	return 1
 
