7da5006c8f623f2d5ffdb39fffec4cb5870104fd
Eddie Kim <eddotman@gmail.com>
2013-12-28 13:04:03 -0500

    csv parsing works

--- pycompute/lib/parse.py
+++ pycompute/lib/parse.py
@@ -1,14 +1,28 @@
 import csv
 import numpy as np 
 
-def parse_data (data, fmt = None):
-	'''parses datafiles given by user'''
+class Parse:
 
-	#check if format is known
-	if fmt == 'csv':
-		pass
-	else:
-		pass
+	@staticmethod
+	def parse_data (datafile, fmt = None):
+		'''parses datafiles given by user'''
 
-	return 1
+		#check if format is known
+		if fmt == 'csv':
+			with open(datafile) as f:
+				reader = csv.reader(f, delimiter=",")
+				return_data = [row for row in reader]
+
+			return return_data
+		else:
+			pass
+
+		return 1
+
+	@staticmethod
+	def test ():
+		print Parse.parse_data('derp.csv', fmt='csv')
+
+if __name__ == '__main__':
+	Parse.test()
 
