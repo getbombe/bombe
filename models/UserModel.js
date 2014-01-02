0d9e042ba90083bf9cae28f43cf6499683c50882
Jack Gao <jackgao2006@gmail.com>
2014-01-02 16:59:43 -0500

    added new user functionaclity

--- models/UserModel.js
+++ models/UserModel.js
@@ -2,16 +2,16 @@ var mongoose	=	require('mongoose'),
 	Schema 		=	mongoose.Schema,
 	ObjectId	=	Schema.ObjectId;
 
-var AgentSchema = new Schema({
+var UserSchema = new Schema({
 	id        			: ObjectId,
 	// title  			   : { type: String, required: true, enum: ['Mr', 'Mrs', 'Mme', 'Miss'] },
 	lastname  			: { type: String, required: true, trim: true},
 	firstname 			: { type: String, required: true, trim: true},
 	email      			: { type: String, required: true, trim: true, index: { unique: true, sparse: true } },
-	company				: String,
+	institution			: String,
 	password			: { type: String, required: true},
 	created_on  : { type : Date, default : Date.now },
 	updated_on  : { type : Date, default : Date.now }
 });	
 
-module.exports = mongoose.model('User', AgentSchema);
+module.exports = mongoose.model('User', UserSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,6 @@
+var UserModel = require('../models/UserModel.js');
+var mongoose = require('mongoose');
+var simpleauth = require('simpleauth');
 
 /*
  * GET users listing.
@@ -11,7 +14,40 @@ var list = function(req, res){
 /*
  * POST user registration
  */
+var register = function(req, res){
+	// connect to MongoDB
+	if (!mongoose.connection.readyState){
+    	mongoose.connect('mongodb://localhost/bombe');
+	}
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
 
+	var email = req.body.email;
+	var firstname = req.body.firstname;
+	var lastname = req.body.lastname;
+	var institution = req.body.institution;
+	var password = simpleauth.generateKey(req.body.password);
+
+	var newUser = new UserModel({
+		email: email,
+		firstname: firstname,
+		lastname: lastname,
+		institution: institution,
+		password: password
+	});
+
+	newUser.save(function (err, result){
+		res.writeHead(200, { 'Content-Type': 'application/json' }); 
+		
+		if(err){
+			res.write("error");
+		} else {
+			res.write("success");
+		}
+
+		res.end();
+	});
+}
 
 
  // export functions
