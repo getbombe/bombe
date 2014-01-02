46fac7d9154289ea7ee75f1653d401b5893e63fa
Eddie Kim <eddotman@gmail.com>
2014-01-02 15:35:48 -0500

    background fully working w/ json

--- pycompute/computerouter.py
+++ pycompute/computerouter.py
@@ -5,20 +5,21 @@ class ComputeRouter:
 	''' static, and should be used that way '''
 	computeMap = {
 		'background_linear': Background.linear,
+		'background_spline': Background.spline,
 		'transform_fourier': Transform.fourier
 	}
 
 	def dataPreprocess(self, data):
-		''' TODO: decide if there is anything here'''
+		''' convert data from JSON '''
 
 		data = json.loads(data)
 		data['data']['x'] = map(float, data['data']['x'])
 		data['data']['y'] = map(float, data['data']['y'])
-		
+
 		return data
 
 	def dataPostprocess(self, data):
-		''' TODO: decide if there is anything here'''
+		''' turn data back into JSON '''
 		data = json.dumps(data)
 		return data
 
@@ -32,6 +33,7 @@ class ComputeRouter:
 if __name__ == '__main__':
 	cr = ComputeRouter()
 	
+	#create dummy json data
 	data = dict()
 	data['data'] = {
 		'x': ['1','2','3','4','5','6'],
@@ -41,4 +43,26 @@ if __name__ == '__main__':
 	data['p2'] = '3'
 	data = json.dumps(data)
 
-	cr.compute('background_linear', data)
+	#linear fit on two points
+	lin = cr.compute('background_linear', data)
+
+	#spline fit
+	spl = cr.compute('background_spline', data)
+	
+	import matplotlib.pyplot as plt 
+	plt.plot(lin['data']['x'], lin['data']['y'])
+	plt.plot(spl['data']['x'], spl['data']['y'])
+	plt.show()
+
+'''
+DATA OBJECT JSON FORMAT
+
+data = {
+	'data' : {
+		'x' : ['1', '2', '3'],
+		'y' : ['4.5', '-3', '0']
+	},
+	'param1' : 'value1',
+	'param2' : 'value2'
+}
+'''
--- pycompute/lib/background.py
+++ pycompute/lib/background.py
@@ -8,22 +8,18 @@ class Background:
 	def spline (data, res=10):
 		'''cubic spline'''
 
-		x_dat = json.loads(data['x'])
-		y_dat = json.loads(data['y'])
+		x_dat = data['data']['x']
+		y_dat = data['data']['y']
 
 		x_itp = list(linspace(x_dat[0], x_dat[len(x_dat)-1], len(x_dat) * res))
 
 		cub_spline = interp1d(x_dat, y_dat, kind='cubic')
 		y_itp = list(cub_spline(x_itp))
 
-		dict = {
-			"x" : x_itp,
-			"y" : y_itp
-		}
+		data['data']['x'] = x_itp
+		data['data']['y'] = y_itp
 
-		ret_json = json.dumps(dict)
-
-		return ret_json
+		return data
 
 
 	@staticmethod
@@ -38,43 +34,8 @@ class Background:
 		coeffs = polyfit(p1, p2, 1)
 		linear = poly1d (coeffs)
 
-		y_fit = linear(data['data']['x'])
+		y_fit = list(linear(data['data']['x']))
 
-		dict = {
-			"x" : list(data['data']['x']),
-			"y" : list(y_fit)
-		}
-
-		return dict
-
-
-	@staticmethod
-	def test():
-
-		import matplotlib.pyplot as plt 
-
-		x = linspace(0, 20)
-		y = array(sin(20*x) + cos(25*x)).tolist()
-
-		data = {
-			'x': json.dumps(list(x)),
-			'y': json.dumps(y)
-		}
-
-		#pt values are indices!
-		pts = {
-			'p1': 1,
-			'p2': 10
-		}
-
-		spl = json.loads(Background.spline(data, res=5))
-		lin = json.loads(Background.linear(data, pts))
-
-		plt.scatter(x, y)
-		plt.plot(spl['x'], spl['y'])
-		plt.plot(lin['x'], lin['y'])
-		plt.show()
-
-
-if __name__ == '__main__':
-	Background.test()
+		data['data']['y'] = y_fit
+		
+		return data
