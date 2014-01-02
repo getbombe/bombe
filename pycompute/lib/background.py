895496fd3d5732021a3d13b21c75122257617fee
Eddie Kim <eddotman@gmail.com>
2014-01-02 13:57:35 -0500

    background json actually working now

--- pycompute/lib/background.py
+++ pycompute/lib/background.py
@@ -8,10 +8,10 @@ class Background:
 	def spline (data, res=10):
 		'''cubic spline'''
 
-		x_dat = data[0]
-		y_dat = data[1]
+		x_dat = json.loads(data['x'])
+		y_dat = json.loads(data['y'])
 
-		x_itp = list(linspace(x_dat[0], x_dat[x_dat.size-1], x_dat.size * res))
+		x_itp = list(linspace(x_dat[0], x_dat[len(x_dat)-1], len(x_dat) * res))
 
 		cub_spline = interp1d(x_dat, y_dat, kind='cubic')
 		y_itp = list(cub_spline(x_itp))
@@ -27,13 +27,13 @@ class Background:
 
 
 	@staticmethod
-	def linear (data, p1, p2):
+	def linear (data, pts):
 		'''given two points, returns the line to subtract'''
 
-		x_dat = data[0]
-		y_dat = data[1]
+		x_dat = json.loads(data['x'])
+		y_dat = json.loads(data['y'])
 
-		coeffs = polyfit([p1[0], p2[0]], [p1[1], p2[1]], 1)
+		coeffs = polyfit([x_dat[pts['p1']], x_dat[pts['p2']]], [y_dat[pts['p1']], y_dat[pts['p2']]], 1)
 		linear = poly1d (coeffs)
 
 		y_fit = list(linear(x_dat))
@@ -54,10 +54,21 @@ class Background:
 		import matplotlib.pyplot as plt 
 
 		x = linspace(0, 20)
-		y = sin(20*x) + cos(25*x)
+		y = array(sin(20*x) + cos(25*x)).tolist()
 
-		spl = json.loads(Background.spline([x,y], res=5))
-		lin = json.loads(Background.linear([x, y], [x[1], y[1]], [x[10], y[10]]))
+		data = {
+			'x': json.dumps(list(x)),
+			'y': json.dumps(y)
+		}
+
+		#pt values are indices!
+		pts = {
+			'p1': 1,
+			'p2': 10
+		}
+
+		spl = json.loads(Background.spline(data, res=5))
+		lin = json.loads(Background.linear(data, pts))
 
 		plt.scatter(x, y)
 		plt.plot(spl['x'], spl['y'])
@@ -66,5 +77,4 @@ class Background:
 
 
 if __name__ == '__main__':
-
 	Background.test()
